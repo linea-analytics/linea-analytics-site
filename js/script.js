c31026974035a112ffd3fb58afc4a9aa4c281561
Claudio Paladini <clode.paladini@gmail.com>
2022-11-21 17:20:34 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+
+    $(".main_panel").scroll(function() {
+        if ($(this).scrollTop() > 0) {
+          $('.scroll_down').fadeOut();
+        } else {
+          $('.scroll_down').fadeIn();
+        }
+      }
+    )
+
+    $(".scroll_down").click(function(){
+      $('.main_panel').animate({
+        scrollTop: $(".row2").offset().top
+    }, 'slow');
+    }) 
+});
